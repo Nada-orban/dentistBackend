461c753a9b336ed3e6ad77c56f2b980c55a0eae3
Nada <nadasamir9334@gmail.com>
2025-05-01 15:18:27 -0400

    add url in  allowed hosts

--- dentist/settings.py
+++ dentist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)hht(o-1i)6x(xf8f4#dn^@id#y1%p!^772npdnlndp*46xq4z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0','localhost','web-production-de0df.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0','localhost','web-production-de0df.up.railway.app',"https://web-production-edf6.up.railway.app/"]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]
