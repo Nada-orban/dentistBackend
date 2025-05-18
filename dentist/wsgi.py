5bc5dcb4dfd7c7666e79f04fd5e5a114c7f0422c
Nada <nadasamir9334@gmail.com>
2025-05-18 14:57:14 -0400

    solve problem of creating admain every time

--- dentist/wsgi.py
+++ dentist/wsgi.py
@@ -10,9 +10,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+
+User = get_user_model()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dentist.settings')
 
 application = get_wsgi_application()
-from django.core.management import call_command
-call_command('createadmin')
+# from django.core.management import call_command
+# call_command('createadmin')
+
+def create_admin_if_needed():
+    if not User.objects.filter(is_superuser=True).exists():
+        call_command('createadmin')
