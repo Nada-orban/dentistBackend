73c7c6d0283b8564088b2288998ffa34cb4d181b
Nada <nadasamir9334@gmail.com>
2025-05-18 15:19:45 -0400

    try

--- dentist/wsgi.py
+++ dentist/wsgi.py
@@ -16,13 +16,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dentist.settings')
 
 application = get_wsgi_application()
-from django.core.management import call_command
-call_command('createadmin')
-# from django.contrib.auth import get_user_model
 # from django.core.management import call_command
+# call_command('createadmin')
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
 
 
-# def create_admin_if_needed():
-#   User = get_user_model()
-#   if not User.objects.filter(is_superuser=True).exists():
-#     call_command('createadmin')
+def create_admin_if_needed():
+  User = get_user_model()
+  if not User.objects.filter(is_superuser=True).exists():
+    call_command('createadmin')
